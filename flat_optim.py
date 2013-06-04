fixed the sign in the feedback projection, things are a lot better now

flat_optim.py
@@ -268,14 +268,14 @@ class Controller():
             self.K = lambda t: np.zeros((m, n))
 
     def __call__(self, t, x):
-        return self.ref.u(t) + np.dot(self.K(t), self.ref.x(t) - x)
+        return self.ref.u(t) - np.dot(self.K(t), self.ref.x(t) - x)
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import time
 
-    tlims = (0, 10)
+    tlims = (0, 2)
 
     """
     t = np.linspace(0, 10, 100)
@@ -293,7 +293,7 @@ if __name__ == "__main__":
 
         ref = trajectory('x', 'u')
         ref.addpoint(0, x=xinit, u=[0, 0])
-        ref.addpoint(10, x=[6, -5, 0, 0], u=[0, 0])
+        ref.addpoint(2, x=[-2, -7, 0, 0], u=[0, 0])
         ref.interpolate()
 
         nlsys = system(s.f, tlims=tlims, xinit=xinit,
@@ -315,11 +315,15 @@ if __name__ == "__main__":
             nlsys.set_u(nucontrol)
             nutraj = nlsys.integrate()
             nutraj.interpolate()
-    """
+    
+    qref = [s.xtopq(ref.x(t)) for t in lintraj._t]
     q = map(s.xtopq, lintraj.x.y)
+    qnu = map(s.xtopq, nutraj.x.y)
 
-    plt.plot([qq[0] for qq in q], [np.sin(qq[0]) for qq in q])
+    plt.plot([qq[0] for qq in q+qnu+qref], 
+             [np.sin(qq[0]) for qq in q+qnu+qref])
+    plt.plot([qq[0] for qq in qref], [qq[1] for qq in qref])
     plt.plot([qq[0] for qq in q], [qq[1] for qq in q])
+    plt.plot([qq[0] for qq in qnu], [qq[1] for qq in qnu])
     plt.axis('equal')
     plt.show()
-    """

nlsymb/aux.py
@@ -1,5 +1,4 @@
 import numpy as np
-#import scipy
 import sympy
 import sympy.core
 import sympy.core.symbol
@@ -74,7 +73,7 @@ def sysIntegrate(func, init,
 
     solver = ode(func, jac)
     solver.set_integrator('vode',
-                          max_step=1e-1, min_step=1e-7,
+                          max_step=1e-1,
                           method=method)
     solver.set_initial_value(init, ti)
 
@@ -92,6 +91,7 @@ def sysIntegrate(func, init,
             if dp*dn < 0:               # if a crossing occured
                 # use interpolation (linear) to find the time
                 # and config at the jump
+                # TODO do a line search instead scipy.optimize.brentq()
                 alpha = dp/(dn-dp)
                 tcross = t[-2] - alpha*(t[-1] - t[-2])
                 xcross = x[-2] - alpha*(x[-1] - x[-2])

nlsymb/lqr.py
@@ -28,7 +28,7 @@ class LQR:
             self.Q = lambda t: np.eye(self.n)
 
         if R is None:
-            self.R = lambda t: 10*np.eye(self.m)
+            self.R = lambda t: 100*np.eye(self.m)
 
         if P is None:
             self.Pf = np.eye(self.n)