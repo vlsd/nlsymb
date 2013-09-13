fixed a sign error in symbsys and added timeout functionality, as well as some error checking and stopping power when armijo goes below a certain step size

flat_optim.py
@@ -20,7 +20,7 @@ def TPlot(tj, fig=None, xlims=(-7,7), clear=False):
         fig = plt.figure()
         #rect = 0.15, 0.1, 0.7, 0.3
         ax = fig.gca(aspect='equal')
-        xlist = np.linspace(*xlims)
+        xlist = np.linspace(*xlims, num=200)
         bound, = ax.plot(xlist, np.sin(xlist), color='red', lw=2)
 
     ax = fig.gca()
@@ -33,14 +33,14 @@ def TPlot(tj, fig=None, xlims=(-7,7), clear=False):
 def quickPlot():
     fig = TPlot(ref)
     TPlot(itj, fig=fig)
-    for tj in trajectories:
-        tj.xtoq(s)
+    for tj in trajectories[-3:]:
+        tj.xtonq(s)
         TPlot(tj, fig=fig)
 
     return fig
 
-
 if __name__ == "__main__":
+
     import matplotlib.pyplot as plt
     import time
     import pickle
@@ -48,9 +48,9 @@ if __name__ == "__main__":
     # the following lines are in order to be able to reload nlsymb in ipython
     # dreload(nlsymb, excludes)
     from IPython.lib.deepreload import reload as dreload
-    excludes = ['time', 'pickle', 'matplotlib.pyplot', 'sys', '__builtin__', '__main__', 'numpy', 'scipy', 'matplotlib', 'os.path', 'sympy', 'scipy.integrate', 'scipy.interpolate', 'nlsymb.sympy']
+    excludes = ['time', 'pickle', 'matplotlib.pyplot', 'sys', '__builtin__', '__main__', 'numpy', 'scipy', 'matplotlib', 'os.path', 'sympy', 'scipy.integrate', 'scipy.interpolate', 'nlsymb.sympy', 'nlsymb.numpy', 'nlsymb.scipy', 'nlsymb.copy', 'copy', 'nlsymb.time']
     
-    tlims = (0, 1)
+    tlims = (0, 2)
 
     """
     t = np.linspace(0, 10, 100)
@@ -66,20 +66,23 @@ if __name__ == "__main__":
         ref_file = open('openlooptj.pkl', 'rb')
         ref = pickle.load(ref_file)
         ref_file.close()
-        ref.xtoq(s)
+        ref.xtonq(s)
         ref.interpolate()
         
         # make an initial guess trajectory
-        qinit = np.array([0, 10])
-        qdoti = np.array([2, 0])
+        qinit = np.array([0.0, 11.0])
+        qdoti = np.array([-1, 0.0])
+
+
         xinit = np.concatenate((s.Psi(qinit),
                                 np.dot(s.dPsi(qinit), qdoti)))
         
         itj = Trajectory('x','u')
         tmid = (tlims[0] + tlims[1])/2
-        itj.addpoint(tlims[0], x=ref.x(tlims[0])*1.1, u=ref.u(tlims[0]))
+        itj.addpoint(tlims[0], x=xinit, u=ref.u(tlims[0]))
+        #itj.addpoint(tlims[0], x=ref.x(tlims[0])*1.1, u=ref.u(tlims[0]))
         #itj.addpoint(tmid, x=ref.x(tmid), u=ref.u(tmid))
-        itj.addpoint(tlims[1], x=ref.x(tlims[1]), u=ref.u(tlims[1]))
+        itj.addpoint(tlims[1], x=ref.x(tlims[1]), u=np.array([0.0,0.0]))
         itj.xtoq(s)
         itj.interpolate()
         
@@ -89,11 +92,10 @@ if __name__ == "__main__":
         nlsys.phi = s.phi
         nlsys.ref = ref
 
-        with Timer("building cost function"):
-            Rcost = lambda t: np.diag([1e-2, 1])
-            Qcost = lambda t: np.diag([10, 10, 1e-3, 1e-3])
-            PTcost = Qcost(2)
-            cost = nlsys.build_cost(R=Rcost, Q=Qcost, PT=PTcost)
+        Rcost = lambda t: np.diag([1, 1])
+        Qcost = lambda t: np.diag([10, 10, 1, 1])
+
+        PTcost = Qcost(2)
         
         #zerocontrol = Controller(reference=ref)
         #nlsys.set_u(zerocontrol)
@@ -102,7 +104,8 @@ if __name__ == "__main__":
         with Timer("initial projection and descent direction"):
             tj = nlsys.project(itj,lin=True)
             trajectories.append(tj)
-        
+       
+            cost = nlsys.build_cost(R=Rcost, Q=Qcost, PT=PTcost)
             descdir = DescentDir(tj, ref, tlims=tlims, cost=cost)
             print("cost of trajectory before descent: %f" %
                   cost(tj))
@@ -111,11 +114,13 @@ if __name__ == "__main__":
                   ddircost)
 
         index = 0
-        while ddircost > 1e-5 :
+        ls = None
+        while ddircost > 1e-2 :
             index = index+1
 
             with Timer("descent direction and line search "):
                 if index is not 1:
+                    cost = nlsys.build_cost(R=Rcost, Q=Qcost, PT=PTcost)
                     descdir = DescentDir(tj, ref, tlims=tlims, cost=cost)
                     print("cost of trajectory before descent: %f" %
                           cost(tj))
@@ -123,12 +128,16 @@ if __name__ == "__main__":
                     print("cost of descent direction: %f" % 
                           ddircost)
 
-                ls = LineSearch(cost, cost.grad, alpha=0.5)
+                if ls is None:
+                    alpha = 100/ddircost
+                else:
+                    alpha = ls.gamma*2
+                ls = LineSearch(cost, cost.grad, alpha=alpha)
                 ls.x = tj
                 ls.p = descdir
                 ls.search()
                 
-                tj += ls.gamma * descdir
+                tj = tj + ls.gamma * descdir
                 print("cost of trajectory after descent: %f" %
                     cost(tj))
 
@@ -150,6 +159,5 @@ if __name__ == "__main__":
     #plt.plot([qq[0] for qq in qnu], [qq[1] for qq in qnu])
     #plt.axis('equal')
     #plt.show()
-
 
 

nlsymb/__init__.py
@@ -5,6 +5,7 @@ import scipy
 from scipy.integrate import ode
 import scipy.interpolate
 from copy import deepcopy
+from timeout import TimeoutError
 
 # from matutils import matmult
 def matmult(*x):
@@ -84,7 +85,7 @@ class Trajectory():
         self.interpolate()
     
     def xtonq(self, s):
-        self._nq = map(s.xtoq, self._x)
+        self._q = map(s.xtoq, self._x)
         self.interpolate()
 
     def __getstate__(self):
@@ -98,7 +99,7 @@ class Trajectory():
 
 
 class LineSearch():
-    def __init__(self, func, grad, alpha=1, beta=1e-8):
+    def __init__(self, func, grad, alpha=1, beta=0*1e-8):
         # func takes a point
         # grad takes a point and a direction
         self.func = func
@@ -109,12 +110,23 @@ class LineSearch():
     def search(self):
         x = self.x
         p = self.p
-        grad = self.grad(x, p)
+        #grad = self.grad(x, p)
+        grad = 1
         func = self.func(x)
         gamma = self.alpha
-        while self.func(x + gamma * p) > func + self.beta * gamma * grad:
-            gamma = gamma/2
-            print("decreasing gamma to %e" % gamma)
+        while True:
+            try:
+                if self.func(x + gamma * p) > \
+                        func + self.beta * gamma * grad:
+                    gamma = gamma/2
+                    print("decreasing gamma to %e" % gamma)
+                    # this will not work with the -O flag
+                    assert gamma > 1e-18, gamma
+                else:
+                    break
+            except TimeoutError:
+                gamma = gamma/10
+                print("Timed out, decreasing gamma to %e" % gamma)
         self.gamma = gamma
 
 

nlsymb/lqr.py
@@ -39,7 +39,8 @@ class LQR:
             self.R = lambda t: Rscale*np.eye(self.m)
 
         if PT is None:
-            self.PT = self.care(self.tf)
+            self.PT = np.eye(self.n)
+            #self.PT = self.care(self.tf)
 
         # this is a Trajectory object
         self.Ptraj = self.cdre()
@@ -132,28 +133,31 @@ class Controller():
         if self.zero:
             return self.ref.u(t)
         else:
-            return self.ref.u(t) + np.dot(self.K(t), self.ref.x(t) - x)
+            return self.ref.u(t) - np.dot(self.K(t), x - self.ref.x(t))
 
 
 class DescentDir(LQR):
     def __init__(self, traj, ref, cost=None, **kwargs):
-        LQR.__init__(self, traj.A, traj.B, PT=cost.PT,
-                     R=cost.R, Q=cost.Q, **kwargs)
+        # not passing R and Q to LQR, getting set to
+        # identity by default
+        LQR.__init__(self, traj.A, traj.B, **kwargs)
 
+        self._cost = cost
         self.traj = traj
         self.ref = ref
         tf = self.tf
-        self.rf = np.dot(self.PT, self.traj.x(tf) - self.ref.x(tf))
-        self.a = lambda t: np.dot(self.Q(t), self.traj.x(t) - self.ref.x(t))
-        self.b = lambda t: np.dot(self.R(t), self.traj.u(t) - self.ref.u(t))
+        P1 = cost.PT
+        self.rf = np.dot(P1, self.traj.x(tf) - self.ref.x(tf))
+        self.a = lambda t: np.dot(cost.Q(t), self.traj.x(t) - self.ref.x(t))
+        self.b = lambda t: np.dot(cost.R(t), self.traj.u(t) - self.ref.u(t))
 
         self.r = self.rsolve()
 
         if 'z0' in kwargs.keys():
             self.z0 = kwargs['z0']
         else:
-            self.z0 = -np.dot(inv(self.P(0)), self.r(0))
-            #self.z0 = np.zeros(self.n)
+            #self.z0 = -np.dot(inv(self.P(0)), self.r(0))
+            self.z0 = np.zeros(self.n)
 
         self.direction = self.solve(self.z0)
 
@@ -183,7 +187,7 @@ class DescentDir(LQR):
         # leaving this here until I change sysIntegrate to handle
         # backwards integration
         solver = ode(rdot)
-        solver.set_integrator('vode', max_step=1e-1, min_step=1e-10)
+        solver.set_integrator('vode', max_step=1e-1, min_step=1e-13)
         solver.set_initial_value(self.rf, -self.tf)
         t = [-self.tf]
         r = [self.rf]
@@ -229,8 +233,7 @@ class DescentDir(LQR):
             expr = matmult(z, self.Q(t), z) + matmult(v, self.R(t), v)
             elist.append(expr)
 
-        out = trapz(elist, tlist) + matmult(self.z(T), self.PT,
-                                            self.z(T))
+        out = trapz(elist, tlist) + matmult(self.z(T), self.PT, self.z(T))
 
         return out
 

nlsymb/sys.py
@@ -7,6 +7,7 @@ from scipy.integrate import trapz
 import tensor as tn
 from . import matmult, interxpolate, sysIntegrate, Trajectory
 from lqr import LQR, Controller
+from timeout import timeout
 
 
 class System():
@@ -110,12 +111,14 @@ class System():
             print("linearizing...")
             self.lintraj = traj
             self.regulator = LQR(traj.A, traj.B, 
-                                 tlims=self.tlims, Rscale=1)
+                                 tlims=self.tlims)
 
         traj.feasible = True
         return traj
+    
 
-    def project(self, traj, tlims=None, Rscale=1, lin=False):
+    @timeout(30)
+    def project(self, traj, tlims=None, lin=False):
         if traj.feasible:
             return traj
 
@@ -141,7 +144,7 @@ class System():
             nutraj = self.integrate(linearize=True)
 
             return self.project(nutraj, tlims=tlims, 
-                                Rscale=Rscale, lin=lin)
+                                lin=lin)
 
 class CostFunction():
     def __init__(self, dimx, dimu, ref,
@@ -290,7 +293,7 @@ class SymSys():
                                      + tn.einsum(
                                          'i,ikl,k', zdot, self.dMz, zdot)/2
                                      + self.dVz
-                                     - np.dot(self._dOhm.T, self.u)
+                                     + np.dot(self._dOhm.T, self.u)
                                      )
                               ))
 
@@ -303,7 +306,7 @@ class SymSys():
 
         out = -tn.einsum('i,ijk,k', zzdot, self.dMzz, zzdot) \
             + tn.einsum('i,ikj,k', zzdot, self.dMzz, zzdot)/2
-        out = np.dot(self.Mzzi, out + self.dVzz - np.dot(self._dOhm.T, self.u))
+        out = np.dot(self.Mzzi, out + self.dVzz + np.dot(self._dOhm.T, self.u))
         out = out - tn.einsum('ijk,j,k',
                                   tn.diff(self._dP, self.z), zdot, zdot)
         # note: dP is the same as dPinverse. woo!