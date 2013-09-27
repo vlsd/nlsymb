dcfcadd447daded48be44b1e7c5126000ac0a66c
Vlad Seghete <vlad.seghete@gmail.com>
2013-09-27 18:53:56 -0500

    trippin t

--- flat_optim.py
+++ flat_optim.py
@@ -142,8 +142,8 @@ if __name__ == "__main__":
         nlsys.phi = s.phi
         nlsys.ref = ref
 
-        Rcost = lambda t: np.diag([1, 1])
-        Qcost = lambda t: np.diag([10, 10, 1, 1])
+        Rcost = lambda t: np.diag([10, 10])
+        Qcost = lambda t: np.diag([100, 100, 1, 1])
 
         PTcost = Qcost(2)
         
