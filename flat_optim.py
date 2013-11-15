jump terms are correct, perhaps barring a sign change. something is funny with descent direction/projection

flat_optim.py
@@ -9,9 +9,10 @@ from sympy import Symbol as S
 import nlsymb
 # nlsymb = reload(nlsymb)
 
-from nlsymb import Timer, LineSearch
+from nlsymb import Timer, LineSearch, np, colored
 from nlsymb.sys import *
 from nlsymb.lqr import *
+
 
 # coming soon to a theatre near you
 # DoublePlot
@@ -122,7 +123,7 @@ if __name__ == "__main__":
                 'nlsymb.scipy', 'nlsymb.copy', 'copy', 'nlsymb.time',
                 'scipy.linalg', 'numpy.linalg']
 
-    tlims = (0, 0.6)
+    tlims = (0, 2)
     ta, tb = tlims
 
     """
@@ -165,7 +166,7 @@ if __name__ == "__main__":
         nlsys.delf = s.delf
 
         Rcost = lambda t: np.diag([10, 10])
-        Qcost = lambda t: np.diag([100, 100, 1, 1])
+        Qcost = lambda t: np.diag([10, 10, 1, 1])
 
         PTcost = Qcost(tb)
 
@@ -189,7 +190,8 @@ if __name__ == "__main__":
             descdir.solve()
 
             costs.append(cost(tj))
-            print("cost of trajectory before descent: %f" % costs[-1])
+            print("cost of trajectory before descent: " +
+                  colored("%f" % costs[-1], 'red'))
 
             ddir = descdir.direction
             ddircost = cost(ddir, project=False)
@@ -212,7 +214,7 @@ if __name__ == "__main__":
                     print("cost of descent direction: %f" % ddircost)
 
                 if ls is None:
-                    alpha = 10000 / ddircost
+                    alpha = 100 / ddircost
                 else:
                     alpha = ls.gamma * 2
                 ls = LineSearch(cost, cost.grad, alpha=alpha, beta=1e-8)

nlsymb/__init__.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sympy as sym
+
 from functools import reduce
 import time
 import scipy
@@ -6,6 +8,7 @@ from scipy.integrate import ode
 import scipy.interpolate
 from copy import deepcopy
 from timeout import TimeoutError
+from termcolor import colored
 
 # from matutils import matmult
 
@@ -19,7 +22,7 @@ def matmult(*x):
 
 
 class Trajectory():
-    # a class to represent a trajectory, takes lists of points and
+    # a class to reyysresent a trajectory, takes lists of points and
     # returns interpolation objects (callables)
 
     def __init__(self, *args):

nlsymb/lqr.py
@@ -3,6 +3,8 @@ from scipy.linalg import schur
 from numpy.linalg import inv
 from scipy.integrate import ode
 from scipy.integrate import trapz
+from IPython.core.debugger import Tracer
+from nlsymb import colored
 
 from . import matmult, sysIntegrate, Trajectory
 
@@ -75,10 +77,12 @@ class CDRE(object):
             self.Pb = kwargs['Pb']
         else:
             # obtain P from an algebraic ricatti at tb
-            care = CARE(self.A(tb), self.B(tb), R=self.R(tb),
-                        Q=self.Q(tb))
-            care.solve()
-            self.Pb = care.P
+            #care = CARE(self.A(ta), self.B(ta), R=self.R(ta),
+            #            Q=self.Q(ta))
+            #care.solve()
+            #self.Pb = care.P
+            #self.Pb = np.eye(n)/100.0
+            self.Pb = np.zeros((n,n))
 
     def _Pdot(self, s, P):
         A, B = self.A(-s), self.B(-s)
@@ -170,14 +174,15 @@ class LQ(LQR):
     def __init__(self, tlims, A, B, **kwargs):
         super(LQ, self).__init__(tlims, A, B, **kwargs)
         # extra stuff
-
+        
         self.q = kwargs['q']
         self.r = kwargs['r']
         self.qf = kwargs['qf']
 
         self.bdot = lambda s, b: self._bdot(s, b)
 
-        #self.tjmp, self.fjmp = map(list, zip(*self.jumps))
+        #if 'jumps' in kwargs:
+        #    self.jumps = kwargs['jumps']
 
     def _bdot(self, s, b):
         A, B = self.A(-s), self.B(-s)
@@ -202,17 +207,19 @@ class LQ(LQR):
 
         while solver.successful() and solver.t < sa:
             solver.integrate(sa, step=True)
-            b = solver.y
+            bb = solver.y
 
-            # I wrongly added this here
-            ## find which jumps lie between this time step and the previous one
-            ## add the corresponding term to b = solver.y + jumpterm
-            #prevtime = np.min(self._bt._t)  # replace with call to _bt.tmin
-            #for (tj, fj) in self.jumps:
-            #    if prevtime > tj and tj > -solver.t:
-            #        b = b + fj * (-solver.t - prevtime)
+            # find which jumps lie between this time step and the previous one
+            # add the corresponding term to b = solver.y + jumpterm
+            #if self.jumps:
+            #    prevtime = np.min(self._bt._t)  # replace with call to _bt.tmin
+            #    for (tj, fj) in self.jumps:
+            #        if prevtime > tj and tj > -solver.t:
+            #            # negative sign because backwards integration
+            #            b = b - matmult(fj.T, b)
+            #            solver.set_initial_value(b, solver.t) 
 
-            self._bt.addpoint(-solver.t, b=b)
+            self._bt.addpoint(-solver.t, b=bb)
 
         self._bt.interpolate()
         self.b = self._bt.b

nlsymb/sys.py
@@ -1,14 +1,14 @@
-import numpy as np
-import sympy as sym
-from sympy import Symbol as S
-from copy import deepcopy
-from scipy.integrate import trapz
+from nlsymb import deepcopy, np, sym, scipy, matmult,\
+        interxpolate, sysIntegrate, Trajectory
 
 import tensor as tn
-from . import matmult, interxpolate, sysIntegrate, Trajectory
+from sympy import Symbol as S
+from scipy.integrate import trapz
+
+#from nlsymb import matmult, interxpolate, sysIntegrate, Trajectory
 from lqr import LQR, Controller
 from timeout import timeout
-
+from IPython.core.debugger import Tracer
 
 class System():
 
@@ -125,7 +125,7 @@ class System():
         traj.jumps = jumps
         return traj
 
-    @timeout(30)
+    @timeout(3000)
     def project(self, traj, tlims=None, lin=False):
         if traj.feasible:
             return traj
@@ -135,7 +135,7 @@ class System():
 
         self.xinit = traj.x(tlims[0])
 
-        if 'regulator' in self.__dict__.keys():
+        if 'regulator' in self.__dict__:
             # print("regular projection")
             ltj = self.lintraj
             reg = self.regulator
@@ -399,11 +399,17 @@ class SymSys():
         # between fplus and fminus at (t, x)
         params = np.concatenate(([t], xval, uval))
         
-        fdiff = self._fplus.func(*params) - self._fmins.func(*params)
 
+        fp = self._fplus.func(*params)
+        fm = self._fmins.func(*params)
+        dphi = self.dphi(xval)
+        
         # this assumes x = [z, zdot]
-        out = np.outer(fdiff, self.dphi(xval))
-        #M = tn.eval(self.Mz, self.z, xval[:self.dim])
+        #print("fdiff at t=%f: %e" % (t, fdiff))
+        out = np.outer(fp-fm, dphi)/np.inner(fp, dphi)
+        M = tn.eval(self.Mz, self.z, xval[:self.dim])
+
+        #Tracer()()
         #out = np.zeros((2*self.dim, 2*self.dim))
         #for i in range(self.dim):
         #    out[self.si, i] = -M[self.si, i]