fixed some problems when calling the cost function on the tangent space; noticed that changing the sign on K in the Controller generates monotonically decreasing descent direction costs

flat_optim.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
                 'nlsymb.scipy', 'nlsymb.copy', 'copy', 'nlsymb.time',
                 'scipy.linalg', 'numpy.linalg']
 
-    tlims = (0, 2)
+    tlims = (0, 0.5)
     ta, tb = tlims
 
     """
@@ -166,9 +166,9 @@ if __name__ == "__main__":
         nlsys.delf = s.delf
 
         Rcost = lambda t: np.diag([10, 10])
-        Qcost = lambda t: np.diag([10, 10, 1, 1])
+        Qcost = lambda t: np.diag([10, 10, 10, 10])
 
-        PTcost = Qcost(tb)
+        PTcost = 0*Qcost(tb)
 
         # zerocontrol = Controller(reference=ref)
         # nlsys.set_u(zerocontrol)
@@ -194,7 +194,7 @@ if __name__ == "__main__":
                   colored("%f" % costs[-1], 'red'))
 
             ddir = descdir.direction
-            ddircost = cost(ddir, project=False)
+            ddircost = cost(ddir, tspace=True)
             gradcosts.append(ddircost)
             print("cost of descent direction: %f" % ddircost)
 
@@ -209,14 +209,14 @@ if __name__ == "__main__":
                     print("cost of trajectory before descent: %f" % costs[-1])
 
                     ddir = descdir.direction
-                    ddircost = cost(ddir, project=False)
+                    ddircost = cost(ddir, tspace=True)
                     gradcosts.append(ddircost)
                     print("cost of descent direction: %f" % ddircost)
 
                 if ls is None:
                     alpha = 100 / ddircost
                 else:
-                    alpha = ls.gamma * 2
+                    alpha = ls.gamma * 10
                 ls = LineSearch(cost, cost.grad, alpha=alpha, beta=1e-8)
                 ls.x = tj
                 ls.p = descdir.direction

nlsymb/lqr.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.linalg import schur
 from numpy.linalg import inv
 from scipy.integrate import ode
-from scipy.integrate import trapz
 from IPython.core.debugger import Tracer
 from nlsymb import colored
 
@@ -248,7 +247,7 @@ class Controller(object):
             self.K = lambda t: np.zeros((m, n))
 
     def __call__(self, t, x):
-        return self.ref.u(t) - \
+        return self.ref.u(t) + \
             matmult(self.K(t), x - self.ref.x(t)) - self.C(t)
 
 

nlsymb/sys.py
@@ -166,15 +166,19 @@ class CostFunction():
         self.PT = PT
         self.projector = (lambda x: x) if projector is None else projector
 
-    def __call__(self, traj, project=True):
-        tj = self.projector(traj) if project else traj
-        T = tj.tlims[1]
+    def __call__(self, traj, tspace=False):
+        tj = traj if traj.feasible or tspace else self.projector(traj) 
+        ta, tb = tj.tlims
+        T = tb
 
-        tlist = tj._t
+        tlist = np.linspace(ta, tb, (tb-ta)*1e3, endpoint=True)
+        xlist = [tj.x(t) for t in tlist]
+        ulist = [tj.u(t) for t in tlist]
         elist = []
-        for (t, x, u) in zip(tlist, tj._x, tj._u):
-            xd = self.ref.x(t)
-            ud = self.ref.u(t)
+        for (t, x, u) in zip(tlist, xlist, ulist):
+            # only consider reference if not in tangent space
+            xd = (0.0 if tspace else 1.0)*self.ref.x(t) 
+            ud = (0.0 if tspace else 1.0)*self.ref.u(t)
             Q = self.Q(t)
             R = self.R(t)
             expr = matmult(x - xd, Q, x - xd) + matmult(u - ud, R, u - ud)
@@ -182,8 +186,10 @@ class CostFunction():
 
         # integrate the above
         out = 0.5 * trapz(elist, tlist)
-        out += 0.5 * matmult(tj.x(T) - self.ref.x(T), self.PT,
-                             tj.x(T) - self.ref.x(T))
+        # don't add a terminal cost in tangent space
+        if not tspace:
+            out += 0.5 * matmult(tj.x(T) - self.ref.x(T), 
+                                 self.PT, tj.x(T) - self.ref.x(T))
         return out
 
     def grad(self, traj, dir):
@@ -406,7 +412,7 @@ class SymSys():
         
         # this assumes x = [z, zdot]
         #print("fdiff at t=%f: %e" % (t, fdiff))
-        out = np.outer(fp-fm, dphi)/np.inner(fp, dphi)
+        out = -np.outer(fp-fm, dphi)/np.inner(fp, dphi)
         M = tn.eval(self.Mz, self.z, xval[:self.dim])
 
         #Tracer()()